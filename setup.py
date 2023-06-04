589c8793df1d25c7302e2f9bc57abc03a6850614
Erika Rowland <130414394+erikareads@users.noreply.github.com>
2023-06-04 11:21:59 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,8 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
  name='DoNothing',
- version='0.0.1',
+ version='0.0.2',
  author='Erika Rowland',
  author_email='accounts@erikarowland.com',
- packages=find_packages('do_nothing'),
+ packages=find_packages(),
 )
