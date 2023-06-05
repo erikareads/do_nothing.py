11e2e6efa07aa8ad162c7cffccb76553db95b8f0
Erika Rowland <accounts@erikarowland.com>
2023-06-04 20:18:44 -0700

    chore: Switch to pyproject.toml

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
- name='DoNothing',
- version='0.0.2',
- author='Erika Rowland',
- author_email='accounts@erikarowland.com',
- packages=find_packages(),
-)
